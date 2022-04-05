7154f29dbf695abb31d091a3394ec842906a154d
tew9ey <tango.tew@ey.com>
2022-04-05 09:30:56 -0500

    initialize the flight service

---
+++ flightservice/src/main/java/com/tango/springcloud/flightservice/FlightserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tango.springcloud.flightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightserviceApplication.class, args);
+	}
+
+}
